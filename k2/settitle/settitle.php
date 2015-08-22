ea7e48f23bdb70732293a0c82c9a1a5f3d61e63e
Ufuk Avcu <ufuk@jooag.de>
2015-08-22 13:56:13 +0200

    Update settitle.php

--- k2/settitle/settitle.php
+++ k2/settitle/settitle.php
@@ -1,18 +1,7 @@
 <?php
-/**
- * @version		2.2
- * @package		Example K2 Plugin (K2 plugin)
- * @author		JoomlaWorks - http://www.joomlaworks.net
- * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
- * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
- */
 
 // no direct access
 defined('_JEXEC') or die ;
-
-/**
- * Example K2 Plugin to render YouTube URLs entered in backend K2 forms to video players in the frontend.
- */
 
 // Load the K2 Plugin API
 JLoader::register('K2Plugin', JPATH_ADMINISTRATOR.'/components/com_k2/lib/k2plugin.php');
